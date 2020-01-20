f9837470e46679f80d76a478d173f85dc02a87c5
jgockley62 <jgockley62@gmail.com>
2020-01-20 21:47:29 +0000

    Gene Name Munger

---
+++ code/modules/01-IGAP_module.R
@@ -0,0 +1,3 @@
+#Load IGAP files
+IGAP <- read.table( file = "/Users/jgockley/Desktop/Projects/AMP-AD_Targets/Candidate_Target_Profiler/RefrenceData/IGAP_stage_1_2_combined.txt", sep = "\t", header = T)
+IGAP1 <- read.table( file = "/Users/jgockley/Desktop/Projects/AMP-AD_Targets/Candidate_Target_Profiler/RefrenceData/IGAP_stage_1.txt", sep = "\t", header = T)
